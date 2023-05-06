ab7a8d67e68210946d114d5c08f7c852c348a88d
hoangnam312 <hoangnam312.dev@gmail.com>
2023-05-06 19:29:34 +0700

    feat: atoms search

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import i18next from 'src/i18n/i18next.config';
+import ASearch from './component/atoms/ASearch/ASearch';
 
 function App() {
 	return (
 		<div>
 			<p className="text-main-color bg-main-background">
 				App {i18next.t('hello')}
+				<ASearch />
 			</p>
 		</div>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: "jit",
-  content: ["src/**/*.{html,ts,tsx}"],
+  content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      "main-background": "#68d69d",
-      "main-color": "#401d83",
+    extend: {
+      colors: {
+        "main-background": "#68d69d",
+        "main-color": "#401d83",
+      },
     },
   },
   plugins: [],
