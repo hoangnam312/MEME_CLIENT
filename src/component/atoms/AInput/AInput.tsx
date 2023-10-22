31aaf4db1d6a0c6a8e45736414ebb1a1711b4701
hoangnam312 <hoangnam312.dev@gmail.com>
2023-10-22 16:44:50 +0700

    fix: AInput controlled component

--- src/component/atoms/AInput/AInput.tsx
+++ src/component/atoms/AInput/AInput.tsx
@@ -1,11 +1,14 @@
 import { IRestParameterAttribute } from 'src/constants/type';
 import AButton from '../AButton/AButton';
+import { useEffect, useState } from 'react';
 
 export interface AInputPropsType {
 	addClass?: string;
 	addClassWrapper?: string;
 	addClassLabel?: string;
+	value?: string;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	onSubmit?: (e: React.FormEvent<HTMLFormElement>, value: string) => void;
 	rest?: IRestParameterAttribute;
 	type?: string;
 	placeholder?: string;
@@ -18,7 +21,9 @@ const AInput = ({
 	addClass = '',
 	addClassWrapper = '',
 	addClassLabel = '',
+	value = '',
 	onChange,
+	onSubmit,
 	rest = {},
 	type = '',
 	placeholder = '',
@@ -26,6 +31,21 @@ const AInput = ({
 	labelButton = '',
 	icon,
 }: AInputPropsType) => {
+	const [valueLocal, setValueLocal] = useState<string>(value);
+
+	const onSubmitDefault = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onSubmit?.(e, valueLocal);
+	};
+
+	const onChangeLocal = (e: React.ChangeEvent<HTMLInputElement>) => {
+		onChange?.(e) ?? setValueLocal(e.target.value);
+	};
+
+	useEffect(() => {
+		setValueLocal(value);
+	}, [value]);
+
 	return (
 		<div className={`${addClassWrapper}`}>
 			{label && (
@@ -37,34 +57,40 @@ const AInput = ({
 				</label>
 			)}
 			<div className="relative">
-				<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-					{icon}
-				</div>
-				<input
-					type={type}
-					className={`block w-full rounded-lg border border-gray-300 
-        bg-gray-50 p-4 pl-10 text-sm 
-        text-gray-900
-        focus-within:border-blue-500 focus-within:ring-blue-500 
-        focus:border-blue-500 focus:ring-blue-500 
-        dark:border-gray-600 dark:bg-gray-700 
-        dark:text-white dark:placeholder-gray-400 
-        dark:focus:border-blue-500 dark:focus:ring-blue-500 
-        ${addClass}`}
-					placeholder={placeholder}
-					required
-					onChange={onChange}
-					{...rest}
-				/>
-				{labelButton && (
-					<div className="absolute bottom-2 right-2.5">
-						<AButton
-							type="submit"
-							addClass="!font-medium text-sm"
-							content={labelButton}
-						/>
+				<form onSubmit={onSubmitDefault}>
+					<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+						{icon}
 					</div>
-				)}
+					<input
+						type={type}
+						className={`block w-full rounded-lg border border-gray-300 
+						bg-gray-50 p-4 pl-10 text-sm 
+						text-gray-900
+						focus-within:border-blue-500 focus-within:ring-blue-500 
+						focus:border-blue-500 focus:ring-blue-500 
+						dark:border-gray-600 dark:bg-gray-700 
+						dark:text-white dark:placeholder-gray-400 
+						dark:focus:border-blue-500 dark:focus:ring-blue-500 
+						${addClass}`}
+						placeholder={placeholder}
+						required
+						value={valueLocal}
+						onChange={onChangeLocal}
+						{...rest}
+					/>
+					{labelButton && (
+						<div className="absolute bottom-2 right-2.5">
+							<AButton
+								addClass="!font-medium text-sm"
+								content={labelButton}
+								rest={{
+									type: 'submit',
+									onClick: onSubmitDefault,
+								}}
+							/>
+						</div>
+					)}
+				</form>
 			</div>
 		</div>
 	);
