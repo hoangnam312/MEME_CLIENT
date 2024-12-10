ea9bf818e6ea6ad0d7e475857569f7d8b5bec77c
hoangnam312 <hoangnam312.dev@gmail.com>
2024-12-10 10:21:34 +0700

    build: add react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { scan } from 'react-scan';
 import { ToastContainer } from 'react-toastify';
 import { RouterProvider } from 'react-router-dom';
 
@@ -9,6 +10,12 @@ import { router } from './routes/root.tsx';
 
 import 'src/assets/css/main.css';
 import 'react-toastify/dist/ReactToastify.css';
+
+// React scan for dev. Ref: https://github.com/aidenybai/react-scan
+scan({
+	enabled: true,
+	log: true, // logs render info to console (default: false)
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
