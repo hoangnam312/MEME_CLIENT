f40d62196f57e87f21383671b3ad137ce06795e7
hoangnam312 <hoangnam312.dev@gmail.com>
2025-01-10 14:32:10 +0700

    reactor: tailwind with portcss and autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,ts,tsx}"],
+  // content: [
+  //   "./index.html",
+  //   "./src/**/*.{ts,tsx}",
+  // ],
   theme: {
     extend: {
       colors: {
