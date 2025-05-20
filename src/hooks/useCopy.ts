ca7f7f13efbbaa22159714d6faef8b6b0fc11933
hoangnam312 <hoangnam312.dev@gmail.com>
2025-05-20 16:32:54 +0700

    refactor: copy image

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -10,35 +10,13 @@ function copyImagePngToClipboard(data: Blob) {
 	navigator.clipboard.write([item]);
 }
 
-async function convertImageToPngByLink(url: string): Promise<string> {
-	return new Promise((resolve, reject) => {
-		const img = new Image();
-		img.setAttribute('crossorigin', 'anonymous');
-		img.onload = () => {
-			const canvas = document.createElement('canvas');
-			const ctx = canvas.getContext('2d');
-			if (!ctx) {
-				reject(new Error('Could not create canvas context'));
-				return;
-			}
-			canvas.width = img.width;
-			canvas.height = img.height;
-			ctx.drawImage(img, 0, 0);
-			resolve(canvas.toDataURL());
-		};
-		img.onerror = reject;
-		img.src = url;
-	});
-}
-
 function useCopyImage() {
 	const [isCopied, setIsCopied] = useState(false);
 	const [isError, setIsError] = useState(false);
 
 	async function copyImage(url: string) {
 		try {
-			const pngImage = await convertImageToPngByLink(url);
-			const imageData = await getImageData(pngImage);
+			const imageData = await getImageData(url);
 			copyImagePngToClipboard(imageData);
 			setIsCopied(true);
 			return true;
