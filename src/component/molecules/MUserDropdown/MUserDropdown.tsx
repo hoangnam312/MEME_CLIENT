728f679ecdc56a5b6b7a3f62ad6a2b6fe3cadb74
hoangnam312 <hoangnam312.dev@gmail.com>
2025-07-27 13:49:47 +0700

    feat(MUserDropdown): user dropdown with avatar display and default icon for guests

--- src/component/molecules/MUserDropdown/MUserDropdown.tsx
+++ src/component/molecules/MUserDropdown/MUserDropdown.tsx
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { t } from 'i18next';
 import { useNavigate } from 'react-router';
 import ADropdown from 'src/component/atoms/ADropdown/ADropdown';
 import { Path } from 'src/constants/type';
 import { useAuthen } from 'src/hooks/useAuthen';
 import useOpen from 'src/hooks/useOpen';
+import { defaultStye } from 'src/assets/css/defaultStyle';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const MUserDropdown = () => {
 	const { logout, isLoggedIn, profile } = useAuthen();
@@ -58,16 +61,26 @@ const MUserDropdown = () => {
 		<div className="flex items-center justify-start">
 			<ADropdown
 				buttonOutside={
-					<div
-						className={`flex h-14 w-14 cursor-pointer items-center justify-center rounded-2xl `}
-						onClick={isOpen ? closeDropdown : openDropdown}
-					>
-						<img
-							src={profile?.avatar || ''}
-							alt={profile?.avatar || 'avatar'}
-							className="rounded-2xl object-cover"
-						/>
-					</div>
+					isLoggedIn() ? (
+						<div
+							className={`flex h-14 w-14 cursor-pointer items-center justify-center rounded-2xl `}
+							onClick={isOpen ? closeDropdown : openDropdown}
+						>
+							<img
+								src={profile?.avatar || ''}
+								alt={profile?.avatar || 'avatar'}
+								className="rounded-2xl object-cover"
+							/>
+						</div>
+					) : (
+						<div
+							className={`flex h-14 w-14 cursor-pointer items-center justify-center 
+					rounded-2xl border-2 border-main-background p-4 ${defaultStye.border}`}
+							onClick={isOpen ? closeDropdown : openDropdown}
+						>
+							<FontAwesomeIcon icon={faUser} size="xl" />
+						</div>
+					)
 				}
 				isOpen={isOpen}
 				options={isLoggedIn() ? options : optionsNoLoggedIn}
